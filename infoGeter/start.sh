4ed8bd5dcde2be5f366501db31417f7b1375d1bf
illuzX <98640218+illuzX@users.noreply.github.com>
2022-06-23 11:49:08 +0530

    Update start.sh

--- infoGeter/start.sh
+++ infoGeter/start.sh
@@ -1,1 +1,19 @@
-
+clear
+echo -e "$Yellow                            processing please wait..>"
+sleep 2.0
+clear
+echo -e "$Yellow                            processing please wait...>"
+sleep 2.0
+clear
+echo -e "$Yellow                            processing please wait....>"
+sleep 2.0
+clear
+echo -e "$Yellow                            processing please wait.....>"
+sleep 2.0
+clear
+echo -e "$Yellow                            processing please wait......>"
+sleep 2.0
+clear
+echo " "
+cd Core_files
+python3 
