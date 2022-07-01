dbb1a48ec8012ea177ede9699c3d891b072cf7da
illuzX <98640218+illuzX@users.noreply.github.com>
2022-07-01 08:58:45 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,8 +4,8 @@ apt-get install figlet
 apt install python
 apt install python2
 apt install git
-pkg install lolcat 
 pip2 install lolcat
+pip3 install -r requirements.txt
 git clone https://github.com/xero/figlet-fonts
 
 
